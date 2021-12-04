d414dcf42fbae0c5d73eb3b0b658fa3ea9432d56
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-04 22:44:18 +0100

    refactor

--- src/Day03.hs
+++ src/Day03.hs
@@ -4,19 +4,24 @@ import Common (Solution(Solution), NoSolution(..), readNum)
 import Data.List.Split (splitOn)
 import Data.Bifunctor (bimap)
 
+solution :: Solution Int Int
 solution = Solution "day03" "Binary Diagnostic" run
 
+run :: String -> (Int, Int)
 run input = let
     numbersList = lines input
     positiveNegativeNumbers = map toPositiveNegative numbersList
     in (powerConsumption positiveNegativeNumbers, lifeSupportRating positiveNegativeNumbers)
 
--- powerConsumption :: [String] -> [Int]
+type Diagnostic = [[Int]]
+
+powerConsumption :: Diagnostic -> Int
 powerConsumption input = let
     gamma = positiveNegativeToBinary $ sumNumbers input
     epsilon = invert gamma
     in toDecimal gamma * toDecimal epsilon
 
+sumNumbers :: Diagnostic -> [Int]
 sumNumbers = foldl1 sumArrays
 
 toPositiveNegative :: String -> [Int]
@@ -42,20 +47,24 @@ toDecimal l = foldl calc 0 $ zip [0..] (reverse l) where
     calc value (coeff, x) = value + 2^coeff * x
 
 
+lifeSupportRating :: Diagnostic -> Int
 lifeSupportRating numbers = let
     oxygenGeneratorRating = rating oxygenGeneratorSelector numbers
     co2ScrubberRating = rating co2ScrubberSelector numbers
     in oxygenGeneratorRating *co2ScrubberRating
 
+rating :: (Int -> Int -> Bool) -> Diagnostic -> Int
 rating selector numbers = let 
     ratingList = head $ snd $ last $ takeWhile (not . null . snd) $ iterate (step selector) (0, numbers)
     in toDecimal $ positiveNegativeToBinary ratingList
--- last $ take (length (head numbers))
 
+oxygenGeneratorSelector :: Int -> Int -> Bool
 oxygenGeneratorSelector sumValue value = if sumValue == 0 then value > 0 else sumValue * value > 0
 
+co2ScrubberSelector :: Int -> Int -> Bool
 co2ScrubberSelector sumValue value = if sumValue == 0 then value < 0 else sumValue * value < 0
 
+step :: (Int -> Int -> Bool) -> (Int, Diagnostic) -> (Int, Diagnostic)
 step selectFunction (pos, numbers) = if pos >= length (head numbers) then (pos, []) else let
     summed = sumNumbers numbers
     valueOnPos = summed !! pos
