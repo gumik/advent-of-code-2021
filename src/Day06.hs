8faae70cc48819273fc2004826406731d258e5b7
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-06 14:15:40 +0100

    day06

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,9 +1,34 @@
+{-# LANGUAGE TupleSections #-}
 module Day06 ( solution ) where
 
 import Common (Solution(Solution), NoSolution(..), readNum)
 import Data.List.Split (splitOn)
 import Data.Bifunctor (bimap)
+import Data.List (iterate', sort)
+import qualified Data.Map.Strict as M
 
-solution = Solution "day06" "" run
+solution :: Solution Int Int
+solution = Solution "day06" "Lanternfish" run
 
-run _ = (NoSolution, NoSolution)
+run :: String -> (Int, Int)
+run inputStr = let
+    input = parse inputStr
+    in (howManyFish 80 input, howManyFish 256 input)
+
+parse :: String -> [Int]
+parse = map readNum . splitOn ","
+
+howManyFish :: Int -> [Int] -> Int
+howManyFish days population = sum $ simulate days (slotsFromPopulation population)
+
+slotsFromPopulation :: [Int] -> [Int]
+slotsFromPopulation = map snd . sort . M.toList . M.unionWith (+) emptySlots . M.fromListWith (+) . map (,1) where
+    emptySlots = M.fromList [(i, 0) | i <- [0..8]]
+
+simulate :: Int -> [Int] -> [Int]
+simulate days slots = iterate simulateDay slots !! days
+
+simulateDay :: [Int] -> [Int]
+simulateDay slots = let
+    [d0, d1, d2, d3, d4, d5, d6, d7, d8] = slots
+    in [d1, d2, d3, d4, d5, d6, d7 + d0, d8, d0]
