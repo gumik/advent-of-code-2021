9c55e408296d8c66d1fa33951e13a7b4671a4df4
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-07 16:57:33 +0100

    day07

--- src/Day07.hs
+++ src/Day07.hs
@@ -4,21 +4,40 @@ import Common (Solution(Solution), NoSolution(..), parseComaSeparatedNums)
 import Data.List.Split (splitOn)
 import Data.Bifunctor (bimap)
 import Data.List (sort)
-import qualified Data.Map.Strict as M
 
--- This is not correct solution, but works for the input. :)
-
+solution :: Solution Int Int
 solution = Solution "day07" "The Treachery of Whales" run
 
+run :: String -> (Int, Int)
 run input = let
     numbers = parseComaSeparatedNums input
-    in (bestCost fuelConsumption1 numbers, bestCost fuelConsumption2 numbers)
+    in (bestCost numbers, bestCost2 numbers)
 
-bestCost :: (Int -> Int) -> [Int] -> Int
-bestCost fuelConsumption nums = minimum $ map (cost fuelConsumption nums) nums
+bestCost :: [Int] -> Int
+bestCost nums = let
+    c1 = cost fuelConsumption1 nums (median nums)
+    c2 = cost fuelConsumption1 nums (median nums + 1)
+    in min c1 c2
 
-cost :: (Int -> Int) -> [Int] -> Int -> Int
-cost fuelConsumption nums m = sum $ map (fuelConsumption . abs . ((-) m)) nums
+bestCost2 :: [Int] -> Int
+bestCost2 nums = let
+    c1 = cost fuelConsumption2 nums (average nums)
+    c2 = cost fuelConsumption2 nums (average nums + 1)
+    in min c1 c2
+
+median :: Integral a => [a] -> a
+median nums = let
+    sorted = sort nums
+    (midIdx, rest) = length nums `divMod` 2
+    in if rest == 0
+        then ((sorted !! midIdx)  +  (sorted !! (midIdx - 1))) `div` 2
+        else sorted !! midIdx
+
+average :: Foldable t => t Int -> Int
+average nums = sum nums `div` length nums
+
+cost :: (Num a, Num b) => (b -> a) -> [b] -> b -> a
+cost fuelConsumption nums m = sum $ map (fuelConsumption . abs . (-) m) nums
 
 fuelConsumption1 :: Int -> Int
 fuelConsumption1 = id
