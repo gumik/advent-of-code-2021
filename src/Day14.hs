fce6a0006d7715f3423cf1734be48d82eda9acad
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-14 08:58:18 +0100

    day14 part2

--- src/Day14.hs
+++ src/Day14.hs
@@ -7,27 +7,43 @@ import Data.Bifunctor (bimap)
 import Control.Arrow ((&&&))
 import qualified Data.Map.Strict as M
 
+type Rules = M.Map String Char
+type PairsCounts = M.Map String Int
+
+solution :: Solution Int Int
 solution = Solution "day14" "" run
 
+run :: String -> (Int, Int)
 run input = let
     (template, rules) = parse input
-    steps = iterate (step rules) template
-    in (minMaxSum $ steps !! 10, 0)
+    pairsCounts = M.fromListWith (+) $ map (,1) $ pairs template
+    steps = iterate (step rules) pairsCounts
+    in (minMaxDiff template $ steps !! 10, minMaxDiff template $ steps !! 40)
 
+parse :: String -> (String, Rules)
 parse input = let
     [template, rulesStr] = splitOn "\n\n" input
     rules = M.fromList $ parseRules rulesStr
     parseRules = map (((!!0) &&& (head . (!!2))) . splitOn " ") . lines
     in (template, rules)
 
-step rules template = case template of
-    (x1:x2:xs) -> x1:(rules M.! [x1,x2]):step rules (x2:xs)
-    [x] -> [x]
-    []  -> []
-
-minMaxSum template = let
-    sums = map snd $ M.toList $ M.fromListWith (+) $ map (,1) template
+minMaxDiff :: String -> PairsCounts -> Int
+minMaxDiff template pairsCounts = let
+    pairsCounts' = M.insertWith (+) [head template, last template] 1 pairsCounts
+    sums = map snd $ M.toList $ M.fromListWith (+) $ concatMap (\(k, v) -> [(head k, v), (k!!1, v)]) $ M.toList pairsCounts'
     min = minimum sums
     max = maximum sums
-    in max - min
+    in (max - min) `div` 2
 
+pairs :: String -> [String]
+pairs template = case template of
+    (x1:x2:xs) -> [x1, x2] : pairs (x2:xs)
+    _          -> []
+
+step :: Rules -> PairsCounts -> PairsCounts
+step rules pairsCounts = M.fromListWith (+) $ concatMap (\(pair, cnt) -> map (,cnt) $ extendPair rules pair) $ M.toList pairsCounts
+
+extendPair :: Rules -> String -> [String]
+extendPair rules pair = case pair of
+    [x1, x2] -> [[x1, mid], [mid, x2]] where mid = rules M.! pair
+    _        -> []
