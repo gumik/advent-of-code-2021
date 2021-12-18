d1d51f46b1c97d31ba615a39b6d944e5a22c9ff1
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-18 16:34:40 +0100

    wip

--- src/Day18.hs
+++ src/Day18.hs
@@ -2,8 +2,37 @@ module Day18 ( solution ) where
 
 import Common (Solution(Solution), NoSolution(..), readNum)
 import Data.List.Split (splitOn)
-import Data.Bifunctor (bimap)
+import Data.Char
+import Control.Monad.State
+import Control.Monad.Loops
+
+data Fish = Number Int | Pair Fish Fish
+type ParseState = State String
 
 solution = Solution "day18" "" run
 
 run _ = (NoSolution, NoSolution)
+
+parseFish :: String -> ParseState Fish
+parseFish = do
+    char <- head <$> get
+    case char of
+        '[' -> parsePair
+        _   -> parseNumber
+
+parsePair :: String -> ParseState Fish
+parsePair = do
+    readChar
+    sub1 <- parseFish
+    readChar
+    sub2 <- parseFish
+    readChar
+    return $ Pair sub1 sub2
+
+parseNumber :: String -> ParseState Fish
+parseNumber = do
+    numStr = untilM readChar $ gets isDigit
+    return $ Number $ readNum numStr
+
+readChar = ParseState Char
+readChar = state . map (head . fst) . splitAt 1
