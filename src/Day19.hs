711b49ba1a451367a7ea743a0c7d460c5047c76f
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-19 12:23:18 +0100

    wip

--- src/Day19.hs
+++ src/Day19.hs
@@ -12,5 +12,5 @@ run input = let
 
 parse = map parseScanner . splitOn "\n\n" where
     parseScanner = map parseLine . tail . lines
-    parseLine l = toTuple . map readNum . splitOn ","
+    parseLine = toTuple . map readNum . splitOn ","
     toTuple [x,y,z] = (x,y,z)
