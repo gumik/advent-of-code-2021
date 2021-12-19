7c38af37734192acb38b8fba860a78672536c493
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-19 21:18:02 +0100

    wip

--- src/Day19.hs
+++ src/Day19.hs
@@ -4,13 +4,22 @@ import Common (Solution(Solution), NoSolution(..), readNum)
 import Data.List.Split (splitOn)
 import Data.Bifunctor (bimap)
 
+type Point = (Int, Int, Int)
+type Scanner = [Point]
+
 solution = Solution "day19" "" run
 
 run input = let
     scanners = parse input
     in (scanners, NoSolution)
 
+parse :: String -> [Scanner]
 parse = map parseScanner . splitOn "\n\n" where
     parseScanner = map parseLine . tail . lines
     parseLine = toTuple . map readNum . splitOn ","
     toTuple [x,y,z] = (x,y,z)
+
+rotX :: Point -> Point
+rotX (x, y, z) = (x, 0, 0)
+
+--  * | 
