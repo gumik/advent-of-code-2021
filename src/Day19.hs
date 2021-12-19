860d38967568645313f2fa3cce5b2c4aabee6d06
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-19 21:26:21 +0100

    wip

--- src/Day19.hs
+++ src/Day19.hs
@@ -20,6 +20,25 @@ parse = map parseScanner . splitOn "\n\n" where
     toTuple [x,y,z] = (x,y,z)
 
 rotX :: Point -> Point
-rotX (x, y, z) = (x, 0, 0)
+rotX (x, y, z) = (x, -z, y)
 
---  * | 
+rotY :: Point -> Point
+rotY (x, y, z) = (z, y, -x)
+
+rotZ :: Point -> Point
+rotZ (x, y, z) = (y, -x, z)
+
+rotateX :: Scanner -> Scanner
+rotateX = map rotX
+
+rotateY :: Scanner -> Scanner
+rotateY = map rotY
+
+rotateZ :: Scanner -> Scanner
+rotateZ = map rotZ
+
+--  . . x | . . .
+--  . . . | . x .
+--  - - - - - - - -    (-1, 2) -> (2, 1) -> (1, -2) -> (-2, -1)
+--  . x . | . . .       (x, y)  -> (y, -x)
+--  . . . | x . .
