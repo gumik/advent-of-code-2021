f5dd6c2aab6adeca5eca8b96bd4c9bc71eb80d57
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-20 12:38:00 +0100

    wip

--- src/Day19.hs
+++ src/Day19.hs
@@ -5,9 +5,15 @@ import Data.List.Split (splitOn)
 import Data.Bifunctor (bimap)
 import qualified Data.Set as S
 import Data.Maybe
+import Data.Foldable (maximumBy)
+import Data.Function (on)
+import qualified Data.Array as A
+import qualified Data.Map as M
 
 type Point = (Int, Int, Int)
+type Offset = Point
 type Scanner = [Point]
+type Rotation = Scanner -> Scanner
 
 solution = Solution "day19" "" run
 
@@ -18,8 +24,9 @@ run input = let
     -- m2 = match (fromJust m1) (scanners !! 4)
     -- m3 = match (fromJust m2) (scanners !! 3)
     -- m4 = match (fromJust m3) (scanners !! 2)
-    x = foldl (\(points, unmatched) scanner -> case match points scanner of Just m -> (m, unmatched); Nothing -> (points, scanner:unmatched)) (S.fromList $ head scanners, []) (tail scanners)
-    in (S.size $ fst x, snd x)
+    matches = filter (isJust . snd . snd) [(s1Idx, (s2Idx, match s1 s2)) | (s1Idx, s1) <- [0..] `zip` scanners, (s2Idx, s2) <- [0..] `zip` scanners, s1 /= s2]
+    matches' = M.fromListWith (++) $ map (\(s1idx, (s2idx, _)) -> (s1idx, [s2idx])) matches
+    in (matches', 0)
 
 parse :: String -> [Scanner]
 parse = map parseScanner . splitOn "\n\n" where
@@ -51,21 +58,35 @@ rotateZ = map rotZ
 --  . x . | . . .       (x, y)  -> (y, -x)
 --  . . . | x . .
 
-rotations :: Scanner -> [Scanner]
-rotations scanner = [angle $ side scanner | side <- sides, angle <- angles] where
+rotations :: [Rotation]
+rotations = [angle . side | side <- sides, angle <- angles] where
     sides = [id, rotateX, rotateX . rotateX, rotateX . rotateX . rotateX, rotateZ, rotateZ . rotateZ . rotateZ]
     angles = [id, rotateY, rotateY . rotateY, rotateY . rotateY . rotateY]
 
-match :: S.Set Point -> Scanner -> Maybe (S.Set Point)
+match :: Scanner -> Scanner -> Maybe (Rotation, Offset)
 match points scanner = let
-    rot = rotations scanner
-    commons = map (maxCommonPoints points) rot :: [(Int, S.Set Point)]
-    commonsWithSize = map (\(commSize, comm) -> (commSize, comm)) commons :: [(Int, S.Set Point)]
-    maxCommon = snd $ maximum commonsWithSize
-    in if S.size maxCommon >= 12 then Just (S.union points maxCommon) else Nothing
+    pointsSet = S.fromList points
+    commons = map (\rotation -> (maxCommonPoints pointsSet (rotation scanner), rotation)) rotations :: [((Int, Point), Rotation)] -- ((overlap cnt, offset), rotation)
+    ((maxOverlapSize, offset), rotation) = maximumBy (compare `on` fst) commons
+    in if maxOverlapSize >= 12 then Just (rotation, offset) else Nothing
 
-maxCommonPoints :: S.Set Point -> [Point] -> (Int, S.Set Point)
+maxCommonPoints :: S.Set Point -> [Point] -> (Int, Point)
 maxCommonPoints p1 p2 = let
-    offsets = S.toList $ S.fromList [(x1 - x2, y1 - y2, z1 - z2) | (x1, y1, z1) <- S.toList p1, (x2, y2, z2) <- p2]
+    offsets = S.toList $ S.fromList [(x1 - x2, y1 - y2, z1 - z2) | (x1, y1, z1) <- S.toList p1, (x2, y2, z2) <- p2] :: [Offset]
     p2offs = map (\(offX, offY, offZ) -> map (\(x, y, z) -> (x+offX, y+offY, z+offZ)) p2) offsets :: [[Point]]
-    in maximum $ map (\(common, p2off) -> (S.size common, S.fromList p2off)) $ map (\p2off -> (S.intersection p1 $ S.fromList p2off, p2off)) p2offs
+    commons = map (\(common, p2off) -> (common, S.fromList p2off)) $ map (\p2off -> (S.intersection p1 $ S.fromList p2off, p2off)) p2offs
+    commons' = filter (\((common, p2off), offset) -> isCorrectOverlap p1 (common, p2off)) (commons `zip` offsets)
+    in maximum $ map (\((common, p2off), offset) -> (S.size common, offset)) commons'
+
+isCorrectOverlap :: S.Set Point -> (S.Set Point, S.Set Point) -> Bool
+isCorrectOverlap p1 (common, p2) = let
+    commonPoints = S.toList common
+    minX = minimum $ map (\(x, _, _) -> x) commonPoints
+    maxX = maximum $ map (\(x, _, _) -> x) commonPoints
+    minY = minimum $ map (\(_, y, _) -> y) commonPoints
+    maxY = maximum $ map (\(_, y, _) -> y) commonPoints
+    minZ = minimum $ map (\(_, _, z) -> z) commonPoints
+    maxZ = maximum $ map (\(_, _, z) -> z) commonPoints
+    p1InCommonRange = filter (\(x, y, z) -> x >= minX && x <= maxX && y >= minY && y <= maxY && z >= minZ && z <= maxZ) $ S.toList p1
+    p2InCommonRange = filter (\(x, y, z) -> x >= minX && x <= maxX && y >= minY && y <= maxY && z >= minZ && z <= maxZ) $ S.toList p2
+    in True --length p1InCommonRange == length commonPoints && length p2InCommonRange == length commonPoints
