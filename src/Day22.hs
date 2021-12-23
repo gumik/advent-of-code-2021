199c9899611fff3fa25d7d3b5a54c2549fed5c48
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-23 10:41:26 +0100

    day22 better algo try

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
-module Day22 ( solution ) where
+module Day22 where
 
 import Common (Solution(Solution), NoSolution(..), readNum, toTuple, toTriple)
 import Data.List.Split (splitOn)
@@ -13,7 +13,7 @@ solution = Solution "day22" "" run
 
 run input = let
     commands = parse input
-    in (cubesOn commands, NoSolution)
+    in (cubesOn commands, cubesOn' commands)
 
 parse = map parseLine . lines where
     parseLine l = let
@@ -41,11 +41,85 @@ cubesOn = S.size . simulateNaiveWay . filter (\(Step _ ((x1, x2), (y1, y2), (z1,
 inSmallRange :: Int -> Bool
 inSmallRange x = x >= -50 && x <= 50
 
+simulateBetterWay :: [Step] -> [Segment]
+simulateBetterWay = foldl step [] where
+    step segments (Step operation ((minX, maxX), (minY, maxY), (minZ, maxZ))) = let
+        segment = Segment minX (maxX+1) [Segment minY (maxY+1) [Segment minZ (maxZ+1) [Unit]]]
+        in case operation of
+            On -> segments `addS` segment
+            Off -> segments `removeS` segment
 
-data Segment = Segment Int Int [Segment] | Unit
+cubesOn' :: [Step] -> Int
+cubesOn' = sum . map area . simulateBetterWay . filter (\(Step _ ((x1, x2), (y1, y2), (z1, z2))) -> all inSmallRange [x1, x2, y1, y2, z1, z2])
 
+data Segment = Segment Int Int [Segment] | Unit deriving (Show, Eq)
+
+-- This may be simplified probably
 add :: [Segment] -> Segment -> [Segment]
 add segments Unit = segments
-add (s'@(Segment a' b' sub'):rest) s@(Segment a b sub)
-    | b <= a'           = s:s':rest
-    | b < b' && a < a'  =
+add [] segment = [segment]
+add (b@(Segment b1 b2 subB):rest) a@(Segment a1 a2 [subA])
+    | a2 <= b1                       = a:b:rest
+    | a2 < b2 && a1 < b1             = Segment a1 b1 [subA] : Segment b1 a2 (add subB subA) : Segment a2 b2 subB : rest
+    | a2 < b2 && a1 == b1            = Segment a1 a2 (add subB subA) : Segment a2 b2 subB : rest
+    | a2 < b2 && a1 > b1             = Segment b1 a1 subB : Segment a1 a2 (add subB subA) : Segment a2 b2 subB : rest
+    | a2 == b2 && a1 < b1            = Segment a1 b1 [subA] : Segment b1 b2 (add subB subA) : rest
+    | a2 == b2 && a1 == b1           = Segment b1 b2 (add subB subA) : rest
+    | a2 == b2 && a1 > b1            = Segment b1 a1 subB : Segment a1 a2 (add subB subA) : rest
+    | a2 > b2 && a1 < b1             = Segment a1 b1 [subA] : Segment b1 b2 (add subB subA) : add rest (Segment b2 a2 [subA])
+    | a2 > b2 && a1 == b1            = Segment b1 b2 (add subB subA) : add rest (Segment b2 a2 [subA])
+    | a2 > b2 && a1 > b1 && a1 < b2  = Segment b1 a1 subB : Segment a1 b2 (add subB subA) : add rest (Segment b2 a2 [subA])
+    | otherwise                      = b : add rest a
+add _ _ = error "unhandled case in add"
+
+--                   a1        a2
+--               b1        b2
+-- remove [Unit] Unit
+remove :: [Segment] -> Segment -> [Segment]
+remove [Unit] Unit = []
+remove segments Unit = segments
+remove [] segment = []
+remove (b@(Segment b1 b2 subB):rest) a@(Segment a1 a2 [subA])
+    | a2 <= b1                       = b:rest
+    | a2 < b2 && a1 < b1             = Segment b1 a2 (remove subB subA) : Segment a2 b2 subB : rest
+    | a2 < b2 && a1 == b1            = Segment a1 a2 (remove subB subA) : Segment a2 b2 subB : rest
+    | a2 < b2 && a1 > b1             = Segment b1 a1 subB : Segment a1 a2 (remove subB subA) : Segment a2 b2 subB : rest
+    | a2 == b2 && a1 < b1            = Segment b1 b2 (remove subB subA) : rest
+    | a2 == b2 && a1 == b1           = Segment b1 b2 (remove subB subA) : rest
+    | a2 == b2 && a1 > b1            = Segment b1 a1 subB : Segment a1 a2 (remove subB subA) : rest
+    | a2 > b2 && a1 < b1             = Segment b1 b2 (remove subB subA) : remove rest (Segment b2 a2 [subA])
+    | a2 > b2 && a1 == b1            = Segment b1 b2 (remove subB subA) : remove rest (Segment b2 a2 [subA])
+    | a2 > b2 && a1 > b1 && a1 < b2  = Segment b1 a1 subB : Segment a1 b2 (remove subB subA) : remove rest (Segment b2 a2 [subA])
+    | otherwise                      = b : remove rest a
+remove _ _ = error "unhandled case in remove"
+
+simplify :: [Segment] -> [Segment]
+simplify [] = []
+simplify [Unit] = [Unit]
+simplify (Segment a b [] : rest) = simplify rest
+simplify [Segment a b sub] = [Segment a b (simplify sub)]
+simplify (a@(Segment a1 a2 subA) : b@(Segment b1 b2 subB) : rest) =
+    if a2 == b1 && subA' == subB'
+        then simplify (Segment a1 b2 subA' : rest)
+        else Segment a1 b1 subA' : simplify (b:rest)
+    where
+        subA' = simplify subA
+        subB' = simplify subB
+simplify _ = error "unhaldled case in simplify"
+
+addS :: [Segment] -> Segment -> [Segment]
+addS s s1 = simplify $ s `add` s1
+removeS :: [Segment] -> Segment -> [Segment]
+removeS s s1 = simplify $ s `remove` s1
+
+area :: Segment -> Int
+area Unit = 1
+area (Segment a b s) = (b - a) * sum (map area s)
+
+
+a = Segment 0 20 [Segment 0 20 [Unit]]
+b = Segment 10 30 [Segment 10 30 [Unit]]
+c = Segment 5 25 [Segment 15 25 [Unit]]
+
+s = [] `add` a `add` b `add` c
+s2 = ([] `add` a `add` b) `remove` c
