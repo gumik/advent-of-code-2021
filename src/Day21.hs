cebf028340c52c2ef4afc5a6af95dde8913cf584
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2021-12-29 15:14:00 +0100

    day21 part 2

--- src/Day21.hs
+++ src/Day21.hs
@@ -1,51 +1,84 @@
 module Day21 ( solution ) where
 
-import Common (Solution(Solution), NoSolution(..), readNum)
+import Common (Solution(Solution), NoSolution(..), readNum, toTuple)
 import Data.List.Split (splitOn)
-import Data.Bifunctor (bimap)
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Control.Monad.State.Strict
 
 data GameState = GameState {
-    _round :: Int,
-    _player1Score :: Int,
-    _player2Score :: Int,
-    _player1Pos :: Int,
-    _player2Pos :: Int,
-    _dice :: [Int],
+    _player1Stat :: PlayerStat,
+    _player2Stat :: PlayerStat,
     _turn :: Turn
-}
-data Turn = Player1Turn | Player2Turn deriving Show
+} deriving (Show, Eq, Ord)
+data Turn = Player1Turn | Player2Turn deriving (Show, Eq, Ord)
+data PlayerStat = PlayerStat {
+    _score :: Int,
+    _pos :: Int
+} deriving (Show, Eq, Ord)
 
-instance Show GameState where
-    show (GameState round p1Score p2Score p1Pos p2Pos dice turn) =
-        "GameState { round = " ++ show round ++
-        ", p1Score = " ++ show p1Score ++
-        ", p2Score = " ++ show p2Score ++
-        ", p1Pos = " ++ show p1Pos ++
-        ", p2Pos = " ++ show p2Pos ++
-        ", dice = " ++ show (head dice) ++
-        ", turn = " ++ show turn ++
-        " }"
-
+solution :: Solution Int Int
 solution = Solution "day21" "Dirac Dice" run
 
-run _ = let
-    p1Pos = 7
-    p2Pos = 10
+run :: String -> (Int, Int)
+run input = let
+    (p1Pos, p2Pos) = parse input
+    initialState = GameState (PlayerStat 0 p1Pos) (PlayerStat 0 p2Pos) Player1Turn
+    in (part1 initialState, part2 initialState)
+
+parse :: String -> (Int, Int)
+parse = toTuple . map (readNum . (!! 4) . splitOn " ") . lines
+
+part1 :: GameState -> Int
+part1 initialState = let
     dice = concat $ repeat [1..100]
-    iterations = iterate game (GameState 0 0 0 p1Pos p2Pos dice Player1Turn)
-    in (part1 iterations, NoSolution)
-
-game :: GameState -> GameState
-game g@(GameState round p1Score p2Score p1Pos p2Pos (x1:x2:x3:xs) turn) = case turn of
-    Player1Turn -> let pos = ((p1Pos + x1 + x2 + x3 - 1) `mod` 10) + 1 in GameState (round + 1) (p1Score + pos) p2Score pos p2Pos xs Player2Turn
-    Player2Turn -> let pos = ((p2Pos + x1 + x2 + x3 - 1) `mod` 10) + 1 in GameState (round + 1) p1Score (p2Score + pos) p1Pos pos xs Player1Turn
-game g = g
-
-anyWin :: GameState -> Bool
-anyWin (GameState _ p1Score p2Score _ _ _ _) = p1Score >= 1000 || p2Score >= 1000
-
-part1 :: [GameState] -> Int
-part1 iterations = let
-    winningIteration = head $ dropWhile (not . anyWin) iterations
-    GameState round p1Score p2Score _ _ _ _ = winningIteration
+    iterations = iterate game (initialState, dice)
+    winningIteration = head $ dropWhile (not . anyWin 1000 . snd) $ zip [0..] $ map fst iterations
+    (round, GameState (PlayerStat p1Score _) (PlayerStat p2Score _) _) = winningIteration
     in 3*round * min p1Score p2Score
+
+game :: (GameState, [Int]) -> (GameState, [Int])
+game (gs, dice) = (step gs (sum $ take 3 dice), drop 3 dice)
+
+step :: GameState -> Int -> GameState
+step (GameState p1 p2 turn) x = case turn of
+    Player1Turn -> GameState (move p1 x) p2 Player2Turn
+    Player2Turn -> GameState p1 (move p2 x) Player1Turn
+
+move :: PlayerStat -> Int -> PlayerStat
+move (PlayerStat score pos) x = PlayerStat (score + pos') pos' where
+    pos' = ((pos + x  - 1) `mod` 10) + 1
+
+anyWin :: Int -> GameState -> Bool
+anyWin score gs = p1Win score gs || p2Win score gs
+
+p1Win :: Int -> GameState -> Bool
+p1Win score (GameState (PlayerStat p1Score _) _ _ ) = p1Score >= score
+
+p2Win :: Int -> GameState -> Bool
+p2Win score (GameState _ (PlayerStat p2Score _) _ ) = p2Score >= score
+
+
+type DiracState = State (M.Map GameState Int)
+
+part2 :: GameState -> Int
+part2 initialState = evalState (p1WinsUniversesCount initialState) M.empty
+
+p1WinsUniversesCount :: GameState -> DiracState Int
+p1WinsUniversesCount gs = do
+    states <- get
+    if p1Win 21 gs then return 1
+    else if p2Win 21 gs then return 0
+    else if gs `M.member` states then return $ states M.! gs
+    else do
+        result <- foldM (accumulateNextStep gs) 0 counts
+        modify $ M.insert gs result
+        return result
+
+accumulateNextStep :: GameState -> Int -> (Int, Int) -> DiracState Int
+accumulateNextStep gs acc (x, cnt) = do
+    rf <- p1WinsUniversesCount (step gs x)
+    return $ acc + cnt * rf
+
+counts :: [(Int, Int)]
+counts = M.toList $ M.fromListWith (+) [(a+b+c,1) | a<-[1..3], b<-[1..3], c<-[1..3]]
